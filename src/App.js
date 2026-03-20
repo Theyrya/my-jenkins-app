63865fb80788dbb65c16a1f82861f2b6754c069b
Dhairya Rana <cloudofdhairya@gmail.com>
2026-03-20 13:46:33 -0600

    test automatic trigger

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>My Jenkins CI/CD Pipeline is working!</code> and save to reload.
         </p>
         <a
           className="App-link"
